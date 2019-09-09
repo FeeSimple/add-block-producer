42cc0454788a8d5dc5a78c50b2a3391c1d4a5d66
trung <trung>
2019-09-09 18:39:02 +0000

    Update script

--- start-new-producer/script_start_producer.sh
+++ start-new-producer/script_start_producer.sh
@@ -1,12 +1,13 @@
-# Script to start a new producer that is connected to the testnet via peer list
+# Script to start a new producer that is connected to the blockchain network via peer list
 
 ## Producer params
-httpServerAddr=127.0.0.1:8000
-p2pListenEndpoint=0.0.0.0:9000
-producerName=trungprod111
-producerKeyPair='["EOS5sxsW17cvjcviWz4U6VSm9Ca84n6KkEbXk23eoX9GqMkYsfqYd","5KSGw8iY8eaVcLxLUTh4CUB2Wzc23afWoWwfKMHNbJUPk5amB7z"]'
-producerPeerList='--p2p-peer-address 138.197.194.220:9000 --p2p-peer-address 138.197.194.220:9001 --p2p-peer-address 138.197.194.220:9002'
+httpServerAddr=0.0.0.0:8001
+p2pListenEndpoint=0.0.0.0:9001
+producerName=prodwallet11
+producerKeyPair='["EOS7d4fQXjJnvsverft9ooGd2YnkzSd1xRLgFAUA6BrwDctUavxtt","5JhV5JtjxfKH8rYmoq75fSa8kJMKexLnB74cC48C1uCvgCwZBzR"]'
+producerPeerList='--p2p-peer-address 165.22.134.182:9000 --p2p-peer-address 165.22.134.182:9001 --p2p-peer-address 165.22.134.182:9002'
 producerPluginList='--plugin eosio::http_plugin --plugin eosio::chain_api_plugin --plugin eosio::producer_plugin'
 
 ## cmd
-./nodeos --max-irreversible-block-age -1 --contracts-console --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./ --http-server-address $httpServerAddr --p2p-listen-endpoint $p2pListenEndpoint --max-clients 14 --p2p-max-nodes-per-host 14 --enable-stale-production --producer-name $producerName --private-key $producerKeyPair $producerPeerList $producerPluginList --http-validate-host false --delete-all-blocks  2>>./stderr &
+/usr/bin/nodeos --max-irreversible-block-age -1 --contracts-console --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./ --http-server-address $httpServerAddr --p2p-listen-endpoint $p2pListenEndpoint --max-clients 14 --p2p-max-nodes-per-host 14 --enable-stale-production --producer-name $producerName --private-key $producerKeyPair $producerPeerList $producerPluginList --http-validate-host false --delete-all-blocks  2>>./stderr &
+
